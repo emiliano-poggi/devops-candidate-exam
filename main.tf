59968bb61e0c70aab42cfef0c17c7bba52722751
Emiliano Poggi <e.141592@gmail.com>
2023-04-20 21:57:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "nat_gateway_route_table" {
 # Create a route to send all traffic to the NAT Gateway
 resource "aws_route" "nat_gateway_route" {
   route_table_id = aws_route_table.nat_gateway_route_table.id
-  destination_cidr_block = "0.0.0.16/0"
+  destination_cidr_block = "10.0.0.0/16"
   nat_gateway_id = data.aws_nat_gateway.nat.id
 }
 
