01e9b4ac7fc77359318b792aad154b62c08b6c3b
Emiliano Poggi <e.141592@gmail.com>
2023-04-20 14:41:39 +0200

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,21 @@
+import json
+import urllib.request
+
+def devopsexam_handler(event, context):
+    url = 'https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data'
+    data = {
+        "subnet_id": "??",
+        "name": "Emiliano Poggi",
+        "email": "emiliano.poggi@siemens.com"
+    }
+    headers = {'X-Siemens-Auth': 'test'}
+    
+    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+    response = urllib.request.urlopen(req)
+    
+    print(response.read())
+    
+    return {
+        'statusCode': response.status,
+        'body': response.read().decode('utf-8')
+    }
