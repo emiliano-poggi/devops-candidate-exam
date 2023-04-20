bda4c66c78876100f51f5a0d9bfd609899139307
Emiliano Poggi <e.141592@gmail.com>
2023-04-20 15:21:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,15 +36,47 @@ resource "aws_security_group" "devopsexam_security_group" {
   vpc_id = aws_vpc.devopsexam.id
 }
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "${path.module}/devopsexam_lambda.zip"
+  source_dir  = "${path.module}"
+  excludes    = [
+    "**/*.tf",
+    "**/*.tfvars",
+    ".terraform/*",
+    ".git/*",
+    ".DS_Store",
+    "*.zip",
+    "*.tar.gz",
+    "*.tar.bz2",
+    "*.tar",
+    "*.gz",
+    "*.bz2",
+    "*.rar",
+    "*.7z",
+    "*.tar.xz",
+    "devopsexam_lambda.py", # Exclude devopsexam_lambda.py from the zip file
+  ]
+}
+
 resource "aws_lambda_function" "devopsexam_lambda" {
   function_name    = "devopsexam_lambda"
   role             = aws_iam_role.lambda_role.arn
   handler          = "devopsexam_lambda.handler"
   runtime          = "python3.8"
-  filename         = "devopsexam_lambda.zip"
-  source_code_hash = filebase64sha256("devopsexam_lambda.zip")
+  filename         = "${path.module}/devopsexam_lambda.zip"
   vpc_config {
     subnet_ids = [aws_subnet.devopsexam_subnet.id]
     security_group_ids = [aws_security_group.devopsexam_security_group.id]
+  }
+}
+
+locals {
+  lambda_code = file("${path.module}/devopsexam_lambda.py")
+}
+
+resource "null_resource" "devopsexam_lambda" {
+  triggers = {
+    lambda_zip = data.archive_file.lambda_zip.output_path
   }
 }
