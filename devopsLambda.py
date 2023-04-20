5ecb4f1a2a2b7301ea4ebf14e3cf09867844cf28
Emiliano Poggi <e.141592@gmail.com>
2023-04-20 20:24:37 +0200

    Update devopsLambda.py

--- devopsLambda.py
+++ devopsLambda.py
@@ -1,2 +1,21 @@
-def handler(event, context):
-    print("Hello, World!")
+import json
+import urllib.request
+
+def lambda_handler(event, context):
+    url = 'https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data'
+    data = {
+        "subnet_id": "xxx",
+        "name": "Emiliano Poggi",
+        "email": "emiliano.poggi@siemens.com"
+    }
+    headers = {'X-Siemens-Auth': 'test'}
+    
+    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+    response = urllib.request.urlopen(req)
+    
+    print(response.read())
+    
+    return {
+        'statusCode': response.status,
+        'body': response.read().decode('utf-8')
+    }
