c6afd465de9f96907eb76fdc3842c92cb92eaa1a
Emiliano Poggi <e.141592@gmail.com>
2023-04-20 19:39:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,6 @@ resource "aws_subnet" "devopsSubnet" {
 resource "aws_security_group" "devopsSecurityGroup" {
   name_prefix = "devopsSecurityGroup"
 
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 65535
